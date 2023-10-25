1053e2a23d31f83b2479ce934685c905676b79bb
funMango <bluemango608@gmail.com>
2023-10-25 15:44:15 +0900

    fix: 맵, 필터, 리듀스 예제12 isLetter를 사용해서 filter사용 기능 추가

--- Grammer/Grammer/MfrEx12.swift
+++ Grammer/Grammer/MfrEx12.swift
@@ -22,3 +22,9 @@ func countAlphabets(_ word: String) -> [String : Int]? {
     return word.unicodeScalars.filter {alphabets.contains($0)}
         .reduce(into: [:]) { $0[String($1), default: 0] += 1}
 }
+
+func countAlphabets2(_ word: String) -> [Character : Int]? {
+    guard word.count > 0 else { return nil }
+    return word.filter {$0.isLetter}
+        .reduce(into: [:]) { $0[$1, default: 0] += 1}
+}
