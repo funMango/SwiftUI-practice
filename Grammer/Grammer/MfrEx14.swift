86cad1d2b686acd2c07ba17ec9073b42f6c7eeee
funMango <bluemango608@gmail.com>
2023-10-25 16:21:20 +0900

    fix: 맵, 필터, 리듀스 예제14 [] + -zsh 배열의 순서를 바꾸는 방법 추가

--- Grammer/Grammer/MfrEx14.swift
+++ Grammer/Grammer/MfrEx14.swift
@@ -14,7 +14,11 @@ func exeMfrEx14() {
 }
 
 func reverse(_ numbers: [Int]) -> [Int] {
-    return numbers.reduce(into: []) { (result, number) in
-        result.insert(number, at: 0)
-    }
+    return numbers.reduce(into: []) { $0.insert($1, at: 0) }            
+}
+
+
+// { [$1] + $0 }: reduce(into:) 클로저 내부에서 각 요소($1)를 새 배열의 맨 앞에 추가합니다.
+func reverse2(_ numbers: [Int]) -> [Int] {
+    return numbers.reduce(into: []) { [$1] + $0 }
 }
