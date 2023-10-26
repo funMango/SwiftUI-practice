e9021556ca97b896258800e833e88ee63b804eac
funMango <bluemango608@gmail.com>
2023-10-26 14:47:03 +0900

    feat: ObservableView timerData 와 ObservableView를 추가해서 초당 count를 증가시키는 기능 구현

---
+++ App/LifeCycleDemo/LifeCycleDemo/Views/ObservableView.swift
@@ -0,0 +1,36 @@
+//
+//  ObservableView.swift
+//  LifeCycleDemo
+//
+//  Created by 이민호 on 10/26/23.
+//
+
+import SwiftUI
+
+struct ObservableView: View {
+    
+    @StateObject var timerData: TimerData = TimerData()
+    
+    var body: some View {
+        
+        NavigationView {
+            VStack {
+                Text("Timer count = \(timerData.timeCount)")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                Button(action: resetCount) {
+                    Text("Reset Counter")
+                }
+            }
+        }
+    }
+    
+    func resetCount() {
+        timerData.resetCount()
+    }
+}
+
+#Preview {
+    ObservableView()
+}
