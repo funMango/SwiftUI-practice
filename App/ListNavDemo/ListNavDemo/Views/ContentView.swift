1e029dc03a7edec87dabd1b65ab79de6c57763cc
funMango <bluemango608@gmail.com>
2023-11-02 10:35:33 +0900

    feat: CarDetail View 각 차의 세부사항을 표시하는 디테일 뷰 구현

--- App/ListNavDemo/ListNavDemo/Views/ContentView.swift
+++ App/ListNavDemo/ListNavDemo/Views/ContentView.swift
@@ -8,19 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @StateObject var carStore : CarStore = CarStore(cars: carData)
-    
+    @StateObject var carStore : CarStore = CarStore(cars: carData)        
     var body: some View {
         List {
             ForEach(0..<carStore.cars.count, id: \.self) { i in
-                HStack {
-                    Image(carStore.cars[i].imageName)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 60)
-                    Text(carStore.cars[i].name)
-                }
+                ListCell(car: carStore.cars[i])
             }
         }
         .padding()
@@ -30,5 +22,19 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ListCell: View {
+    var car: Car
+    
+    var body: some View {
+        HStack {
+            Image(car.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 60)
+            Text(car.name)
+        }
     }
 }
