c83db226b1458babcdb049d086d0a1663fd98635
funMango <bluemango608@gmail.com>
2023-11-08 17:35:08 +0900

    feat(UI(Grid)): LazyVgrid, LazyHgrid 연습

---
+++ App/UI/UI/Views/Grid/Grid.swift
@@ -0,0 +1,48 @@
+//
+//  Grid.swift
+//  UI
+//
+//  Created by 이민호 on 2023/11/08.
+//
+
+import SwiftUI
+
+struct CellContent: View {
+    var index: Int
+    var color: Color
+    
+    var body: some View {
+        Text("\(index)")
+            .frame(minWidth: 75, minHeight: 50, maxHeight: .infinity)
+            .background(color)
+            .cornerRadius(8)
+            .font(.system(.largeTitle))
+    }
+}
+
+struct Grid: View {
+    private var colors: [Color] = [.blue, .yellow, .green]
+    private var gridItems = [
+        GridItem(.fixed(150)),
+        GridItem(.adaptive(minimum: 50)),
+        GridItem(.fixed(150)),
+    ]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: gridItems, spacing: 5) {
+                ForEach((0...99), id: \.self) { index in
+                    CellContent(index: index, color: colors[index % colors.count])
+                    
+                }
+            }
+            .padding(5)
+        }
+    }
+}
+
+struct Grid_Previews: PreviewProvider {
+    static var previews: some View {
+        Grid()
+    }
+}
