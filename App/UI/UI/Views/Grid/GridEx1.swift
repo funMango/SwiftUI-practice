09bc1fedcf981ae36f010a350c4a327af060bc51
funMango <bluemango608@gmail.com>
2023-11-08 17:52:02 +0900

    feat(UI(Grid)): gridEx1

---
+++ App/UI/UI/Views/Grid/GridEx1.swift
@@ -0,0 +1,45 @@
+//
+//  GridEx1.swift
+//  UI
+//
+//  Created by 이민호 on 2023/11/08.
+//
+
+import SwiftUI
+
+
+struct GridEx1: View {
+    @State private var value: Double = 1.0 // 기본값 1로 설정
+    var gridItems: [GridItem] {
+        Array(repeating: .init(.flexible()), count: Int(value))
+    }
+
+    var body: some View {
+        VStack {
+            Slider(value: $value, in: 1...5, step: 1)
+            LazyVGrid(columns: gridItems, spacing: 5) {
+                ForEach(1...5, id: \.self) { index in
+                    Content(index: index)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct Content: View {
+    let index: Int
+
+    var body: some View {
+        Text("\(index)")
+            .frame(minWidth: 50, minHeight: 50)
+            .background(Color.blue)
+            .cornerRadius(10)
+    }
+}
+
+struct GridEx1_Previews: PreviewProvider {
+    static var previews: some View {
+        GridEx1()
+    }
+}
